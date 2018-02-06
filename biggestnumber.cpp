64d9cf1d7c1193c7e9685497259d72b095afd190
ssalshamsi <ssalshamsi@mail.csuchico.edu>
2018-02-06 12:10:53 -0800

    Learned biggest number

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,12 +5,37 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
+
+int number1;
+
+int number2;
+
+int biggest;
+
   cout<<"Please enter a whole number:\n";
   
+  cin >> number1;
+
   cout<<"Please enter another whole number:\n";
 
-  cout<<"Of those two numbers, the biggest is: ";
+  cin >> number2;  
+
+ 
+if ( number1 > number2 ) 
+{
+
+    biggest=number1; 
+
+}
+else  
+{
+
+    biggest=number2;
+
+}
+
+cout <<"of those two numbers, the biggest is: "<<biggest;
+
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
